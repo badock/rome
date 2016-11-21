31ca57782ff3c6c005c69e4d62a30ad670ec0144
Jonathan Pastor <jonathan.pastor@me.com>
2016-11-20 20:57:37 -0600

    use 'unittest' for tests;

--- test/v2/join_compatibility.py
+++ test/v2/join_compatibility.py
@@ -1,14 +1,27 @@
-from sqlalchemy.orm import joinedload, joinedload_all, subqueryload, subqueryload_all
+import logging
+import unittest
+
+from sqlalchemy.orm import joinedload, subqueryload
 
 import rome_models
 import sqlalchemy_models
-from test.compatibility.utils import get_literal_query
+
 
 def init_mock_objects(models_module):
     models_module.drop_tables()
     models_module.create_tables()
     models_module.init_objects()
 
+
+def extract_column(x):
+    return getattr(x, "__tablename__", str(x))
+
+
+def extract_row(x):
+    if hasattr(x, "__iter__"):
+        return map(lambda y: extract_column(y), x)
+    else:
+        return extract_column(x)
 
 """ It should implement the behaviour depicted in
 http://docs.sqlalchemy.org/en/latest/orm/loading_relationships.html """
@@ -17,11 +30,6 @@ http://docs.sqlalchemy.org/en/latest/orm/loading_relationships.html """
 def testing_join_1(query_module):
     models_module = sqlalchemy_models
     query = query_module.get_query(models_module.Author).join(models_module.Book)
-    if query_module is sqlalchemy_models:
-        print(query.column_descriptions)
-        print get_literal_query(query)
-    extract_column = lambda x: getattr(x, "__tablename__", str(x))
-    extract_row = lambda x: map(lambda y: extract_column(y), x) if hasattr(x, "__iter__") else extract_column(x)
 
     rows = query.all()
     result = map(extract_row, rows)
@@ -31,11 +39,6 @@ def testing_join_1(query_module):
 def testing_join_2(query_module):
     models_module = sqlalchemy_models
     query = query_module.get_query(models_module.Author, models_module.Book).join(models_module.Book)
-    if query_module is sqlalchemy_models:
-        print(query.column_descriptions)
-        print get_literal_query(query)
-    extract_column = lambda x: getattr(x, "__tablename__", str(x))
-    extract_row = lambda x: map(lambda y: extract_column(y), x) if hasattr(x, "__iter__") else extract_column(x)
 
     rows = query.all()
     result = map(extract_row, rows)
@@ -45,11 +48,6 @@ def testing_join_2(query_module):
 def testing_join_3(query_module):
     models_module = sqlalchemy_models
     query = query_module.get_query(models_module.Author).join(models_module.Author.books)
-    if query_module is sqlalchemy_models:
-        print(query.column_descriptions)
-        print get_literal_query(query)
-    extract_column = lambda x: getattr(x, "__tablename__", str(x))
-    extract_row = lambda x: map(lambda y: extract_column(y), x) if hasattr(x, "__iter__") else extract_column(x)
 
     rows = query.all()
     result = map(extract_row, rows)
@@ -60,11 +58,6 @@ def testing_join_4(query_module):
     models_module = sqlalchemy_models
     query = models_module.get_query(models_module.Author).join(models_module.Author.books).options(
         joinedload(models_module.Author.books))
-    if query_module is sqlalchemy_models:
-        print(query.column_descriptions)
-        print get_literal_query(query)
-    extract_column = lambda x: getattr(x, "__tablename__", str(x))
-    extract_row = lambda x: map(lambda y: extract_column(y), x) if hasattr(x, "__iter__") else extract_column(x)
 
     rows = query.all()
     result = map(extract_row, rows)
@@ -76,11 +69,6 @@ def testing_join_5(query_module):
     query = query_module.get_query(models_module.Author).join(models_module.Author.books).options(
         joinedload(models_module.Author.books)).filter(models_module.Author.name == 'Author3').filter(
         models_module.Book.title == 'Book3_5')
-    if query_module is sqlalchemy_models:
-        print(query.column_descriptions)
-        print get_literal_query(query)
-    extract_column = lambda x: getattr(x, "__tablename__", str(x))
-    extract_row = lambda x: map(lambda y: extract_column(y), x) if hasattr(x, "__iter__") else extract_column(x)
 
     rows = query.all()
     result = map(extract_row, rows)
@@ -92,11 +80,6 @@ def testing_join_6(query_module):
     query = query_module.get_query(models_module.Author, models_module.Author.name).join(models_module.Author.books).options(
         subqueryload(models_module.Author.books)).filter(models_module.Author.name == 'Author3').filter(
         models_module.Book.title == 'Book3_5')
-    if query_module is sqlalchemy_models:
-        print(query.column_descriptions)
-        print(get_literal_query(query))
-    extract_column = lambda x: getattr(x, "__tablename__", str(x))
-    extract_row = lambda x: map(lambda y: extract_column(y), x) if hasattr(x, "__iter__") else extract_column(x)
 
     rows = query.all()
     result = map(extract_row, rows)
@@ -108,21 +91,59 @@ def compare(function, model_a, model_b):
     result_b = function(model_b)
 
     if result_a != result_b:
-        print("ERROR: %s != %s" % (result_a, result_b))
+        logging.error("ERROR: %s != %s" % (result_a, result_b))
+        return False
     else:
-        print("Success!")
+        return True
+
+DATA_INITIALIZED = False
 
 
-if __name__ == '__main__':
-    # Init objects
-    init_mock_objects(sqlalchemy_models)
-    init_mock_objects(rome_models)
+class TestJoinOperation(unittest.TestCase):
 
-    # Basic join
-    compare(testing_join_1, sqlalchemy_models, rome_models)
-    compare(testing_join_2, sqlalchemy_models, rome_models)
-    compare(testing_join_3, sqlalchemy_models, rome_models)
-    compare(testing_join_4, sqlalchemy_models, rome_models)
-    compare(testing_join_5, sqlalchemy_models, rome_models)
-    compare(testing_join_6, sqlalchemy_models, rome_models)
+    def setUp(self):
+        global DATA_INITIALIZED
+        if not DATA_INITIALIZED:
+            print("setUp")
+            DATA_INITIALIZED = True
+            init_mock_objects(sqlalchemy_models)
+            init_mock_objects(rome_models)
+
+    def test_joining_compatibility_1(self):
+        comparison_result = compare(testing_join_1, sqlalchemy_models, rome_models)
+        self.assertEquals(comparison_result, True)
+
+    def test_joining_compatibility_2(self):
+        comparison_result = compare(testing_join_2, sqlalchemy_models, rome_models)
+        self.assertEquals(comparison_result, True)
+
+    def test_joining_compatibility_3(self):
+        comparison_result = compare(testing_join_3, sqlalchemy_models, rome_models)
+        self.assertEquals(comparison_result, True)
+
+    def test_joining_compatibility_4(self):
+        comparison_result = compare(testing_join_4, sqlalchemy_models, rome_models)
+        self.assertEquals(comparison_result, True)
+
+    def test_joining_compatibility_5(self):
+        comparison_result = compare(testing_join_5, sqlalchemy_models, rome_models)
+        self.assertEquals(comparison_result, True)
+
+    def test_joining_compatibility_6(self):
+        comparison_result = compare(testing_join_6, sqlalchemy_models, rome_models)
+        self.assertEquals(comparison_result, True)
+
+
+# if __name__ == '__main__':
+#     # Init objects
+#     init_mock_objects(rome_models)
+#     init_mock_objects(sqlalchemy_models)
+#
+#     # Basic join
+#     compare(testing_join_1, sqlalchemy_models, rome_models)
+#     compare(testing_join_2, sqlalchemy_models, rome_models)
+#     compare(testing_join_3, sqlalchemy_models, rome_models)
+#     compare(testing_join_4, sqlalchemy_models, rome_models)
+#     compare(testing_join_5, sqlalchemy_models, rome_models)
+#     compare(testing_join_6, sqlalchemy_models, rome_models)
 
